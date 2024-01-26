67c279d50d9613e01207832f74bcadf0db28d366
Awet <awet@Awets-MacBook.local>
2024-01-26 02:53:31 +0100

    remove the code added for test

--- application/src/Controller/PublicController.php
+++ application/src/Controller/PublicController.php
@@ -5,29 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 
+#[Route('/', name: 'public_')]
 class PublicController extends AbstractController
 {
-    #[Route('/', name: 'app_public')]
-    public function index(MailerInterface $mailer): Response
+    #[Route('/', name: 'home')]
+    public function home(): Response
     {
-        $email = (new Email())
-            ->from('hello@example.com')
-            ->to('you@example.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
-
-        $mailer->send($email);
-
-        return $this->render('public/index.html.twig', [
-            'controller_name' => 'PublicController',
-        ]);
+        return $this->render('public/home.html.twig');
     }
 }
